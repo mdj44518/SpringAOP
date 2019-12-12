b420d88445f2415ba46d0b941aea367e68a780dc
mond <esj44518@naver.com>
2019-12-12 23:23:15 +0900

    git branch 5

--- aop/src/main/java/test/spring_main/MainApp.java
+++ aop/src/main/java/test/spring_main/MainApp.java
@@ -26,6 +26,7 @@ public class MainApp {
 		System.out.println("브랜치 이어서 작업 1");
 		System.out.println("브랜치 이어서 작업 2");
 		System.out.println("브랜치 이어서 작업 3");
+		System.out.println("브랜치 이어서 작업 4");
 	}
 
 }
